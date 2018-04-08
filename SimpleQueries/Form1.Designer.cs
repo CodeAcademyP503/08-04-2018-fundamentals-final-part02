6faf202ab042847b184dd38a1a541179906ec95e
Samir Kerimov <samir.karimov@code.edu.az>
2018-04-08 18:40:17 +0400

    Simple queries

--- SimpleQueries/Form1.Designer.cs
+++ SimpleQueries/Form1.Designer.cs
@@ -28,33 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.RichTextBox();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.Search = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // Result
             // 
-            this.button1.Location = new System.Drawing.Point(149, 111);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Result.Location = new System.Drawing.Point(12, 330);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(765, 96);
+            this.Result.TabIndex = 0;
+            this.Result.Text = "";
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(13, 13);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(158, 20);
+            this.searchBox.TabIndex = 1;
+         
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(185, 10);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(75, 23);
+            this.Search.TabIndex = 2;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.Result);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox Result;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Button Search;
     }
 }
 
--- SimpleQueries/Form1.cs
+++ SimpleQueries/Form1.cs
@@ -12,9 +12,140 @@ namespace SimpleQueries
 {
     public partial class Form1 : Form
     {
+        public int Count = 0;
+       
+        public List<Student> StudentList = new List<Student>()
+        {
+            new Student("Elshad","Rustemov",21),
+            new Student("Ilkin","Xaniyev",29),
+            new Student("Adigozel","Xelilzade",19),
+            new Student("Nermin","Rustemova",25),
+            new Student("Emrah","Nagiyev",24),
+            new Student("Ferid","Gulmemmedov",28),
+            new Student("Togrul","Babayev",28),
+            new Student("Nicat","Eliyev",29),
+            new Student("Turkan","Nesirli",20),
+            new Student("Anar","Abishov",27),
+            new Student("Tebriz","Bedelov",24),
+            new Student("Shahin","Cabbarov",20),
+            new Student("Elnur","Soltanov",26),
+            new Student("Nicat","Quliyev",24),
+            new Student("Nigar","Shukurova",20),
+            new Student("Elgiz","Eyvazli",18),
+            new Student("Nicat","Sadixov",17),
+            new Student("Atilla","Rzazade",15),
+        };
+        
+        
         public Form1()
         {
             InitializeComponent();
+            
+           
+            
+        }
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+            //  1. Soyadi "Bedelov" olan telebenin Adi ve Yasi
+            /*
+             var keyword = searchBox.Text;
+            foreach (Student item in StudentList)
+            {
+
+                if (item.Name == keyword)
+                {
+                    Result.Text = item.Surname + " - " + item.Age;
+                    break;
+                }
+                else
+                {
+                    Result.Text = "Bazada bele melumat yoxdur";
+                }
+            }
+            */
+
+            //  2.Adi "Nicat" olan telebelerin sayi
+            /*
+            foreach (Student item in StudentList)
+            {
+
+                if (item.Name == "Nicat")
+                {
+                    Count++;
+                    
+                }
+               
+            }
+            Result.Text = Count.ToString();
+            */
+
+            //3. Adi "Nicat" olan telebelerden yasi 23 den boyuk olanlarin soyadi ve yasi
+            /*
+            foreach (Student item in StudentList)
+            {
+
+                if (item.Name == "Nicat" && item.Age>23)
+                {
+                    Result.Text += item.Surname +"\r\n";
+
+                }
+
+            }*/
+
+            // 4.Yasi 24 den boyuk olan telebelerin siyahisi
+            /*foreach (Student item in StudentList)
+            {
+
+                if (item.Age > 24)
+                {
+                    Result.Text += item.Name + "\r\n";
+
+                }
+
+            }
+            */
+            //5.Soyadi en uzun olan telebenin adi
+            /* int ageCount = StudentList.Count;
+            int[] ages = new int[ageCount];
+            for (int i = 0; i < StudentList.Count; i++)
+            {
+                ages[i] = StudentList[i].Surname.Length;
+            }
+            foreach (var item in StudentList)
+            {
+                if (item.Surname.Length == ages.Max())
+                {
+                    Result.Text = item.Name;
+                }
+            }
+            */
+            // 6.Sinifin yas ortalamasi
+            /*
+            foreach (Student item in StudentList)
+            {
+
+                Count += item.Age;
+
+            }
+            Result.Text = (Count / StudentList.Count).ToString();
+            */
+        }
+
+
+    }
+    public class Student
+    {
+       
+        public string Name;
+        public string Surname;
+        public int Age;
+        public Student(string _name,string _surname,int _age)
+        {
+            Name = _name;
+            Surname = _surname;
+            Age = _age;
         }
     }
+
 }
